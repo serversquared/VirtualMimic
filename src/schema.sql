6e445ccfc23f19ee8dc9b10e1ee5b69af02e4137
Shelvacu <shelvacu@gmail.com>
2015-11-08 08:54:42 +0000

    added indexes

--- src/schema.sql
+++ src/schema.sql
@@ -14,3 +14,8 @@ CREATE TABLE IF NOT EXISTS nodes_to_nodes
 );
 
 
+CREATE INDEX IF NOT EXISTS awesome_index
+ON nodes (`parent_id`,`order`);
+
+CREATE INDEX IF NOT EXISTS cool_index
+ON nodes_to_nodes (`input`, `response`);
