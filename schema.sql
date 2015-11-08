664e78d84eb804bd215e7c1ea0a160ba561228ab
Shelvacu <shelvacu@gmail.com>
2015-11-08 04:52:58 +0000

    insert into db lib

--- schema.sql
+++ schema.sql
@@ -1,14 +1,16 @@
 CREATE TABLE IF NOT EXISTS nodes
 (
-`parent` bigint,
+`parent_id` bigint,
 `type` varchar(10),
-`order` int
+`order` int,
+`word` varchar(20)
 );
 
 CREATE TABLE IF NOT EXISTS node_to_node
 (
 `input` bigint,
-`response` bigint
+`response` bigint,
+`weight` double
 );
 
 
